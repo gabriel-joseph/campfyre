bedbc94e60c3b13d97fea250344f7a5b19e45922
Gabriel Vance <ilovenarwhales@gmail.com>
2024-10-12 21:07:45 -0500

    lol

---
+++ module 5 - data structures and algos/step 1 - basics.js
@@ -0,0 +1,48 @@
+// arrays - ordered list of values
+const listOfNumbers = [1, 3, 5, 7, 9];
+
+console.log("for loop");
+
+/**
+ * For loop
+ * i is the counter
+ * Starts at 0 to include the first index
+ * Loops through the array using i < listOfNumbers.length to prevent out-of-bounds errors
+ */
+for (let i = 0; i < listOfNumbers.length; i++) {
+  console.log(`${listOfNumbers[i]}'s index is ${i}`);
+}
+
+console.log("\nfor...of loop");
+
+/**
+ * for...of loop
+ * Iterates directly over array values
+ * No index is required
+ */
+for (const num of listOfNumbers) {
+  console.log(`${num} is in the array`);
+}
+
+console.log("\nforEach loop");
+
+/**
+ * forEach loop
+ * Cleaner syntax, index is optional
+ */
+listOfNumbers.forEach((num, index) =>
+  console.log(`${num}'s index is ${index}`)
+);
+
+console.log("\nwhile loop");
+
+let counter = 0;
+/**
+ * While loop
+ * Continues as long as the counter is less than the array's length
+ */
+while (counter < listOfNumbers.length) {
+  console.log(`${listOfNumbers[counter]}'s index is ${counter}`);
+  counter += 1;
+}
+
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Purpose of course
+
+Provide basic knowledge of software engineering for entry-mid level skillset
+
+# Why Java, JavaScript, and Python?
+
+These are 3 of the most popular and versatile languages. They are constantly sought after in jobs and can be used for a large variety of tasks.
+
+# Syllabus:
+
+* Course basics/goals (i.e. this doc)
+* Intro to languages/software
+* Java
+* JavaScript
+* Python
+** Basics
+*** data types
+*** classes
+*** control flow
+**
+
+
+
+
+
