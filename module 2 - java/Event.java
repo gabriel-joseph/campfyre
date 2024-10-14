613a5c6413cd6d1d9fb0191e82246a422b4cdd9a
Gabriel Vance <ilovenarwhales@gmail.com>
2024-10-14 06:54:16 -0500

    lol

---
+++ module 2 - java/Event.java
@@ -0,0 +1,19 @@
+
+
+
+public class Event {
+
+    boolean allAges; // true or false
+    int attendance; 
+    float temp;
+    long temp2;
+    double temp3;
+    byte someByte;
+    char A;
+
+    String nameOfEvent;
+
+    // public static void main(String[] args) { // psvm
+    //     System.out.println(isFirstClass); // sysout
+    // }
+}
